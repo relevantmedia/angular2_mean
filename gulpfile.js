dfc4a14e009f2adaf1054aa98472aed1e0ab24e1
Ryan Strandt <ryan.strandt@cru.org>
2015-05-12 16:59:02 -0400

    loading gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
-var typescript = require('gulp-tsc');
+var gulp = require('gulp'),
+	defaultAssets = require('./config/assets/default'),
+	plugins = require('gulp-load-plugins')();
 
-gulp.task('ts', function () {
-	gulp.src([])
-	.pipe(typescript())
+gulp.task('tsc', function () {
+	gulp.src([defaultAssets.client.ts])
+	.pipe(plugins.tsc)
 	.pipe(gulp.dest('dest/'))
 });
 
+// Sass task
+gulp.task('sass', function () {
+	return gulp.src(defaultAssets.client.sass)
+		.pipe(plugins.sass())
+		.pipe(plugins.rename(function (path) {
+			path.dirname = path.dirname.replace('/scss', '/css');
+		}))
+		.pipe(gulp.dest('./modules/'));
+});
